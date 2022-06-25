30ba9f0077f4b17a67a396a396b615bf6b8fee23
MrTest <hitsjt@gmail.com>
2022-06-25 14:45:57 +0800

    -

--- aes.py
+++ aes.py
@@ -15,89 +15,95 @@ class AESCryptor(object):
         self.key = key
         self.iv = iv
 
-    def cbc_encrypt(self, data, with_pkcs7padding=True):
-        cipher = Cipher(algorithms.AES(self.key), modes.CBC(self.iv), backend=default_backend())
-        if with_pkcs7padding:
-            data = self.pkcs7_padding(data)
-        return cipher.encryptor().update(data)
-
-    def cbc_decrypt(self, data, with_pkcs7padding=True):
-        cipher = Cipher(algorithms.AES(self.key), modes.CBC(self.iv), backend=default_backend())
-        data = cipher.decryptor().update(data)
-        if with_pkcs7padding:
-            data = self.pkcs7_unpadding(data).decode()
-        return data
-
-    def ecb_encrypt(self, data, with_pkcs7padding=True):
-        cipher = Cipher(algorithms.AES(self.key), modes.ECB(), backend=default_backend())
-        if with_pkcs7padding:
-            data = self.pkcs7_padding(data)
-        return cipher.encryptor().update(data)
-
-    def ecb_decrypt(self, data, with_pkcs7padding=True):
-        cipher = Cipher(algorithms.AES(self.key), modes.ECB(), backend=default_backend())
-        data = cipher.decryptor().update(data)
-        if with_pkcs7padding:
-            data = self.pkcs7_unpadding(data).decode()
-        return data
-
-    def cfb_encrypt(self, data, with_pkcs7padding=True):
-        cipher = Cipher(algorithms.AES(self.key), modes.CFB(self.iv), backend=default_backend())
-        if with_pkcs7padding:
-            data = self.pkcs7_padding(data)
-        return cipher.encryptor().update(data)
-
-    def cfb_decrypt(self, data, with_pkcs7padding=True):
-        cipher = Cipher(algorithms.AES(self.key), modes.CFB(self.iv), backend=default_backend())
-        data = cipher.decryptor().update(data)
-        if with_pkcs7padding:
-            data = self.pkcs7_unpadding(data).decode()
-        return data
-
-    def ctr_encrypt(self, data, with_pkcs7padding=True):
-        cipher = Cipher(algorithms.AES(self.key), modes.CTR(self.iv), backend=default_backend())
-        if with_pkcs7padding:
-            data = self.pkcs7_padding(data)
-        return cipher.encryptor().update(data)
-
-    def ctr_decrypt(self, data, with_pkcs7padding=True):
-        cipher = Cipher(algorithms.AES(self.key), modes.CTR(self.iv), backend=default_backend())
-        data = cipher.decryptor().update(data)
-        if with_pkcs7padding:
-            data = self.pkcs7_unpadding(data).decode()
-        return data
-
-    def ofb_encrypt(self, data, with_pkcs7padding=True):
-        cipher = Cipher(algorithms.AES(self.key), modes.OFB(self.iv), backend=default_backend())
-        if with_pkcs7padding:
-            data = self.pkcs7_padding(data)
-        return cipher.encryptor().update(data)
-
-    def ofb_decrypt(self, data, with_pkcs7padding=True):
-        cipher = Cipher(algorithms.AES(self.key), modes.OFB(self.iv), backend=default_backend())
-        data = cipher.decryptor().update(data)
-        if with_pkcs7padding:
-            data = self.pkcs7_unpadding(data).decode()
-        return data
-
-    def gcm_encrypt(self, data, associated_data, with_pkcs7padding=True):
+    def ecb_encrypt(self, data):
         """
-        GCM mode is not completed because of tag, result may not correct.
+        ECB mode should have pkcs padding with default
+        """
+        cipher = Cipher(algorithms.AES(self.key), modes.ECB(), backend=default_backend())
+        data = self.pkcs7_padding(data)
+        return cipher.encryptor().update(data)
+
+    def ecb_decrypt(self, data):
+        """
+        ECB mode should have pkcs padding with default
+        """
+        cipher = Cipher(algorithms.AES(self.key), modes.ECB(), backend=default_backend())
+        data = cipher.decryptor().update(data)
+        return self.pkcs7_unpadding(data).decode()
+
+    def cbc_encrypt(self, data):
+        """
+        CBC mode should have pkcs padding with default
+        """
+        cipher = Cipher(algorithms.AES(self.key), modes.CBC(self.iv), backend=default_backend())
+        data = self.pkcs7_padding(data)
+        return cipher.encryptor().update(data)
+
+    def cbc_decrypt(self, data):
+        """
+        CBC mode should have pkcs padding with default
+        """
+        cipher = Cipher(algorithms.AES(self.key), modes.CBC(self.iv), backend=default_backend())
+        data = cipher.decryptor().update(data)
+        return self.pkcs7_unpadding(data).decode()
+
+    def cfb_encrypt(self, data):
+        cipher = Cipher(algorithms.AES(self.key), modes.CFB(self.iv), backend=default_backend())
+        return cipher.encryptor().update(data)
+
+    def cfb_decrypt(self, data):
+        cipher = Cipher(algorithms.AES(self.key), modes.CFB(self.iv), backend=default_backend())
+        return cipher.decryptor().update(data)
+
+    def ofb_encrypt(self, data):
+        cipher = Cipher(algorithms.AES(self.key), modes.OFB(self.iv), backend=default_backend())
+        return cipher.encryptor().update(data)
+
+    def ofb_decrypt(self, data):
+        cipher = Cipher(algorithms.AES(self.key), modes.OFB(self.iv), backend=default_backend())
+        return cipher.decryptor().update(data)
+
+    def ctr_encrypt(self, data):
+        cipher = Cipher(algorithms.AES(self.key), modes.CTR(self.iv), backend=default_backend())
+        return cipher.encryptor().update(data)
+
+    def ctr_decrypt(self, data):
+        cipher = Cipher(algorithms.AES(self.key), modes.CTR(self.iv), backend=default_backend())
+        return cipher.decryptor().update(data)
+
+    def gcm_encrypt(self, data, associated_data):
+        """
+        This GCM combines enc_data and tag, thus no independent tag value here
         """
         aesgcm = AESGCM(self.key)
-        if with_pkcs7padding:
-            data = self.pkcs7_padding(data)
         return aesgcm.encrypt(self.iv, data, associated_data)
 
-    def gcm_decrypt(self, data, associated_data, with_pkcs7padding=True):
+    def gcm_decrypt(self, data, associated_data):
         """
-        GCM mode is not completed because of tag, result may not correct.
+        This GCM combines enc_data and tag, thus no independent tag value here
         """
         aesgcm = AESGCM(self.key)
-        data = aesgcm.decrypt(self.iv, data, associated_data)
-        if with_pkcs7padding:
-            data = self.pkcs7_unpadding(data).decode()
-        return data
+        return aesgcm.decrypt(self.iv, data, associated_data)
+
+    def gcm_encrypt_withtag(self, data, associated_data):
+        """
+        This GCM has standard return value with enc_data and tag verify
+        """
+        encryptor = Cipher(algorithms.AES(self.key), modes.GCM(self.iv), backend=default_backend()).encryptor()
+        encryptor.authenticate_additional_data(associated_data)
+        ciphertext = encryptor.update(data)
+        encryptor.finalize()
+        return ciphertext, encryptor.tag
+
+    def gcm_decrypt_withtag(self, data, associated_data, tag):
+        """
+        This GCM has standard return value with enc_data and tag verify
+        """
+        decryptor = Cipher(algorithms.AES(self.key), modes.GCM(self.iv), backend=default_backend()).decryptor()
+        decryptor.authenticate_additional_data(associated_data)
+        plaintext = decryptor.update(data)
+        decryptor.finalize_with_tag(tag)
+        return plaintext
 
     @staticmethod
     def pkcs7_padding(data):
